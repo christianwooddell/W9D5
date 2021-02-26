779fe1c338d453b948faf750d471e977fd0440bc
Christian <cwooddel@u.rochester.edu>
2021-02-26 17:36:57 -0500

    finish strong

--- skeleton 2/src/drop_down.js
+++ skeleton 2/src/drop_down.js
@@ -6,6 +6,6 @@ const dogs = {
   "American Staffordshire Terrier": "https://www.akc.org/dog-breeds/american-staffordshire-terrier/",
   "Tosa": "https://www.akc.org/dog-breeds/tosa/",
   "Labrador Retriever": "https://www.akc.org/dog-breeds/labrador-retriever/",
-  "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
+  "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/"
 };
 
